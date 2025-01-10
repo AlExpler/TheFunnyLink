15d582107e5f6af3f94e5c6a065ed5d8804dee90
AlExpler <help.ftsgroup@gmail.com>
2025-01-10 22:32:59 +0300

    Create main.js

---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+let links = ["https://www.desmos.com/calculator/srpq2jkbcj", "https://youtu.be/dQw4w9WgXcQ?si=FLyieLvX5_ibLwrB"];
+let rand = Math.floor(Math.random() * 2);
+location.replace(links[rand]);
