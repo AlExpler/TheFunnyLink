8cbb1e7068d6c65a1a2af01c22d89fd7f0876fe8
AlExpler <help.ftsgroup@gmail.com>
2025-03-12 13:37:13 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-let links = ["https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads/main/src/images/sticker.webp",
-             "https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads/main/src/images/sticker2.webp",
-             "https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads/main/src/images/sticker3.webp",
-             "https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads/main/src/images/sticker4.webp"];
+let links = ["https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads/main/src/images/sticker.png",
+             "https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads/main/src/images/sticker2.png",
+             "https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads/main/src/images/sticker3.png",
+             "https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads/main/src/images/sticker4.jpg"];
 let rand = Math.floor(Math.random() * 2);
 location.replace(links[rand]);
