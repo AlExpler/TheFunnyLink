760ec4912c3f3170c6e51c3ad8c34dae5dbe6fa7
AlExpler <help.ftsgroup@gmail.com>
2025-03-12 13:37:54 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,5 @@ let links = ["https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads
              "https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads/main/src/images/sticker2.png",
              "https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads/main/src/images/sticker3.png",
              "https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads/main/src/images/sticker4.jpg"];
-let rand = Math.floor(Math.random() * 2);
+let rand = Math.floor(Math.random() * 4);
 location.replace(links[rand]);
