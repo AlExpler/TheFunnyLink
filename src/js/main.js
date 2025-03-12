124a7ed9ee1456b5270f1cb31448b4b440275284
AlExpler <help.ftsgroup@gmail.com>
2025-03-12 13:35:31 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
-let links = ["https://www.desmos.com/calculator/srpq2jkbcj", "https://youtu.be/dQw4w9WgXcQ?si=FLyieLvX5_ibLwrB"];
+let links = ["https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads/main/src/images/sticker.webp",
+             "https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads/main/src/images/sticker2.webp",
+             "https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads/main/src/images/sticker3.webp",
+             "https://raw.githubusercontent.com/AlExpler/TheFunnyLink/refs/heads/main/src/images/sticker4.webp"];
 let rand = Math.floor(Math.random() * 2);
 location.replace(links[rand]);
